7e76abbc7106802f540b24411d0b2a4f7d6e8b56
strahinja ajvaz <strahinja.ajvaz@gmail.com>
2019-12-12 22:39:04 +1100

    completed the bare bone structure

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -6,12 +6,39 @@ const StyledCardItem = styled.div`
   display: inline-block;
   text-align: center;
 
+  p.name {
+    height: 40px;
+  }
+
   p.retailPrice {
     text-decoration: line-through;
   }
 
   p.salePrice {
     font-size: 3.2rem;
+  }
+
+  & > div.imageContainer {
+    position: relative;
+    & > div {
+      opacity: 0;
+      position: absolute;
+      top: 0;
+      width: 100%;
+      height: 100%;
+      transition: all 0.3s ease-in-out;
+
+      & > * {
+        position: relative;
+        bottom: 0;
+      }
+    }
+
+    &:hover > div {
+      opacity: 1;
+      background-color: rgba(0, 0, 0, 0.4);
+      transition: all 0.3s ease-in-out;
+    }
   }
 `;
 
@@ -20,22 +47,32 @@ const CardItem = memo(function CardItem({
   itemInCart,
   addToCart
 }) {
-  console.log(quantityAvailable);
   return (
     <StyledCardItem>
-      <img src={imageUrl} alt={name} />
-      <p>{name}</p>
+      <div className="imageContainer">
+        <img src={imageUrl} alt={name} />
+        <div>
+          {itemInCart && quantityAvailable > 0 && (
+            <button
+              onClick={() =>
+                addToCart({
+                  id,
+                  name,
+                  quantity: 1,
+                  salePrice,
+                  quantityAvailable
+                })
+              }
+            >
+              Add to cart
+            </button>
+          )}
+          {quantityAvailable === 0 && <p>Sold out</p>}
+        </div>
+      </div>
+      <p className="name">{name}</p>
       <p className="retailPrice">{(retailPrice / 100).toFixed(2)}</p>
       <p className="salePrice">{(salePrice / 100).toFixed(2)}</p>
-      {itemInCart && (
-        <button
-          onClick={() =>
-            addToCart({ id, name, quantity: 1, salePrice, quantityAvailable })
-          }
-        >
-          Add to cart
-        </button>
-      )}
     </StyledCardItem>
   );
 });
--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -9,8 +9,14 @@ import {
   removeItemFromShoppingCart
 } from "../actions/shoppingCart";
 import { ShoppingCartItem } from "./ShoppingCartItem";
+import { cursor } from "sisteransi";
 
-const StyledShoppingCart = styled.div``;
+const StyledShoppingCart = styled.div`
+  border: 1px solid grey;
+  margin-top: 20px;
+  margin-right: 20px;
+  padding: 20px 15px;
+`;
 
 function ShoppingCart({
   shoppingCartItems,
@@ -21,20 +27,35 @@ function ShoppingCart({
   return (
     <StyledShoppingCart>
       <p>Shopping Cart</p>
-      {shoppingCartItems.map(
-        ({ id, name, quantity, quantityAvailable, salePrice }) => (
-          <ShoppingCartItem
-            key={id}
-            id={id}
-            name={name}
-            itemQuantity={quantity}
-            quantityAvailable={quantityAvailable}
-            salePrice={salePrice}
-            incrementQuantity={incrementQuantity}
-            decrementQuantity={decrementQuantity}
-            removeItem={removeItemFromCart}
-          />
-        )
+      {shoppingCartItems.length === 0 && <p>You have no items in your cart</p>}
+      {shoppingCartItems.length > 0 && (
+        <>
+          {shoppingCartItems.map(
+            ({ id, name, quantity, quantityAvailable, salePrice }) => (
+              <ShoppingCartItem
+                key={id}
+                id={id}
+                name={name}
+                itemQuantity={quantity}
+                quantityAvailable={quantityAvailable}
+                salePrice={salePrice}
+                incrementQuantity={incrementQuantity}
+                decrementQuantity={decrementQuantity}
+                removeItem={removeItemFromCart}
+              />
+            )
+          )}
+          <p>
+            Total:
+            {shoppingCartItems
+              .reduce((acc, cur) => {
+                const totalForItem = (cur.quantity * cur.salePrice) / 100;
+                acc += totalForItem;
+                return acc;
+              }, 0)
+              .toFixed(2)}
+          </p>
+        </>
       )}
     </StyledShoppingCart>
   );
